83d819f78ae2b6d190c9f2def97e13df82471ce4
junil15 <junilg15@gmail.com>
2022-09-26 15:11:09 +0900

    Main.java 수정 #3

--- Main.java
+++ Main.java
@@ -1,1 +1,1 @@
-public class Main { public static void main(String[] args) {String s = "hello world"; System.out.println(s);}
+public class Main { public static void main(String[] args) { String s = "안녕하세요"; System.out.println(s); } }
